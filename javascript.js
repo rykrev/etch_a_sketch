b2d996a2b0c5f36b0c62e014256540b5e44f0430
rykrev <hugo773344@gmail.com>
2024-01-19 10:14:57 -0500

    updated grid function, fixing styling and scripting

--- javascript.js
+++ javascript.js
@@ -1,32 +1,55 @@
-const big = document.querySelector(".big")
+const big = document.querySelector(".big");
 
-const TOP_LEFT = document.createElement("div")
-for (i = 0; i < 4; i++) {
-    let newDiv_TL = document.createElement("div")
-    newDiv_TL.style.cssText = "display: block; color: red;"
-    TOP_LEFT.appendChild(newDiv_TL)
-}
-const TOP_RIGHT = document.createElement("div")
-for (i = 0; i < 4; i++) {
-    let newDiv_TR = document.createElement("div")
-    newDiv_TR.style.cssText = "display: block; color: blue;"
-    TOP_RIGHT.appendChild(newDiv_TR)
-}
-const BOTTOM_LEFT = document.createElement("div")
-for (i = 0; i < 4; i++) {
-    let newDiv_BL = document.createElement("div")
-    newDiv_BL.style.cssText = "display: block; color: yellow;"
-    BOTTOM_LEFT.appendChild(newDiv_BL)
-}
-const BOTTOM_RIGHT = document.createElement("div")
-for (i = 0; i < 4; i++) {
-    let newDiv_BR = document.createElement("div")
-    newDiv_BR.style.cssText = "display: block; color: orange;"
-    BOTTOM_RIGHT.appendChild(newDiv_BR)
+function createRow(size) {
+    for (let j = 0; j < size; j++) {
+        let newRow = document.createElement("div");
+        newRow.classList.add("cell-row");
+        newRow.style.height = 600 / size + "px";
+        let columns = document.querySelectorAll(".column");
+        columns[j-1].appendChild(newRow);
+        big.appendChild(columns)
+    }
 }
 
-big.appendChild(TOP_LEFT)
-big.appendChild(TOP_RIGHT)
-big.appendChild(BOTTOM_LEFT)
-big.appendChild(BOTTOM_RIGHT)
+function createColumn(size) {
+    for (let i = 0; i < size; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("column");
+        newDiv.style.width = 600 / size + "px";
+        big.appendChild(newDiv);
+        createRow(size);
+    }
+}
+
+createColumn(16);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
