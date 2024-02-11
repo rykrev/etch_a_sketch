95c98735279fdc31be747bedb6506a65e7245cca
rykrev <hugo773344@gmail.com>
2024-02-10 23:53:35 -0500

    finished application with random color changes and refresh button

--- javascript.js
+++ javascript.js
@@ -1,29 +1,48 @@
 const big = document.querySelector(".big");
+const refresh = document.querySelector(".refresh")
 
-function createRow(size) {
-    for (let j = 0; j < size; j++) {
-        let newRow = document.createElement("div");
-        newRow.classList.add("cell-row");
-        newRow.style.height = 600 / size + "px";
-        let columns = document.querySelectorAll(".column");
-        columns[j-1].appendChild(newRow);
-        big.appendChild(columns)
+refresh.style.width = "100px";
+refresh.style.height = "45px";
+refresh.textContent = "Refresh?"
+
+refresh.addEventListener("click", () => {
+    let dimensions = prompt("How many squares do you want on both sides? (e.g. 25 for a 25 x 25 grid)");
+    if (dimensions >= 100) {
+        alert("Please do not pass 100 squares.");
+        return;
+    }
+    else {
+        grid(dimensions);
+}})
+
+function grid(size) {
+    big.replaceChildren();
+    for (i = 0; i < size; i++) {
+        const column = document.createElement("div");
+        column.classList.add("column");
+        column.style.width = "60px";
+        for (j = 0; j < size; j++) {
+            const row = document.createElement("div");
+            row.classList.add("row");
+            row.addEventListener("mouseover", () => {
+                const randomColor = Math.floor(Math.random()*16777215).toString(16);
+                row.style.backgroundColor = `#` + `${randomColor}`;
+            })
+            row.style.height = "50px";
+            row.style.border = "2px solid black";
+            column.appendChild(row);
+        }
+        big.appendChild(column)
     }
 }
 
-function createColumn(size) {
-    for (let i = 0; i < size; i++) {
-        let newDiv = document.createElement("div");
-        newDiv.classList.add("column");
-        newDiv.style.width = 600 / size + "px";
-        big.appendChild(newDiv);
-        createRow(size);
+function clear() {
+    for (i=0; i < document.childNodes; i++) {
+        document.removeChild(document.childNodes[i])
     }
 }
 
-createColumn(16);
-
-
+grid(16);
 
 
 
